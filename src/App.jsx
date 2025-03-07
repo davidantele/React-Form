c8c28afb9dcf2ee297b7a261721a9222b3fea6eb
davidantele <davidantele1@gmail.com>
2025-03-06 20:52:28 -0600

    2

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+import Authenticate from "./components/Authenticate";
+import SignUpForm from "./components/SignUpForm";
+
+export default function App() {
+  const [token, setToken] = useState(null);
+
+  return (
+    <>
+      <SignUpForm token={token} setToken={setToken} />
+
+      <Authenticate token={token} setToken={setToken} />
+    </>
+  );
+}
